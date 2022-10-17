fb41ea26779a78348e0d5776caff047ac4c282a4
123mjb <micahbutt@outlook.com>
2022-10-17 21:51:29 +0100

    working but solver not with all the features

--- binairosolver.py
+++ binairosolver.py
@@ -2,7 +2,7 @@ def solve(inp:list[list[int]]):
     for i in range(0,len(inp)):
         for g in range(0,len(inp[i])):
             if inp[i][g] == 3:
-                if len(inp[i])-g >1:
+                if len(inp[i])-1-g>=2:
                     if inp[i][g+1] == 1 and inp[i][g+2] == 1:
                         inp[i][g] = 0
                         if len(inp[i])-g >2 and inp[i][g+3] == 3:
@@ -11,7 +11,7 @@ def solve(inp:list[list[int]]):
                         inp[i][g] = 1
                         if len(inp[i])-g >2 and inp[i][g+3] == 3:
                             inp[i][g+3] = 1
-                if len(inp)-i >1:
+                if len(inp)-i-1 >=2:
                     if inp[i+1][g] == 1 and inp[i+2][g] == 1:
                         inp[i][g] = 0
                         if len(inp)-i >2 and inp[i+3][g] == 3:
@@ -25,7 +25,7 @@ def solve(inp:list[list[int]]):
                         inp[i][g] = 0
                     if inp[i][g-1] == 0 and inp[i][g+1] == 0:
                         inp[i][g] = 1
-                if i > 1 and len(inp) - 1 - i > 0:
+                if i > 0 and len(inp) - 1 - i > 0:
                     if inp[i-1][g] == 1 and inp[i+1][g] == 1:
                         inp[i][g] = 0
                     if inp[i-1][g] == 0 and inp[i+1][g] == 0:
@@ -36,13 +36,14 @@ def printer(output:list[list[int]]):
     for i in range(0,len(output[0])):
         for g in range(0,len(output)):
             printout += str(output[g][i])
-        printout += "\n"
+        if i < len(output[0])-1:
+            printout += "\n"
     print(printout)
 
 
-printer(solve([
+"""printer(solve([
     [3,1,3,3],
     [1,0,0,1],
     [1,0,0,3],
     [3,1,3,3]
-]))
+]))"""
--- binairosolverinput.py
+++ binairosolverinput.py
@@ -1,1 +1,39 @@
 import pygame
+from pygame import *
+from binairosolver import *
+
+pygame.init()
+DISPLAY_SIZE = tuple(map(lambda i, j: i - j, pygame.display.get_desktop_sizes()[0], (10,50)))
+DISPLAYSURF = pygame.display.set_mode(tuple(DISPLAY_SIZE),pygame.RESIZABLE)
+board = [[3,3,3,3,3,3,3,3],[3,3,3,3,3,3,3,3],[3,3,3,3,3,3,3,3],[3,3,3,3,3,3,3,3],[3,3,3,3,3,3,3,3],[3,3,3,3,3,3,3,3],[3,3,3,3,3,3,3,3],[3,3,3,3,3,3,3,3]]
+colours = [pygame.Color(0,0,0),pygame.Color(255,255,255),pygame.Color(255,0,0)]
+WHITE = pygame.Color(255,255,255)
+BLACK = pygame.Color(0,0,0)
+RED = pygame.Color(255,0,0)
+DISPLAYSURF.fill(WHITE)
+running = True
+set = 0
+mouseloc: tuple[int,int]
+while running:
+    for i in range(0,len(board)):
+        for g in range(0,len(board[i])):
+            pygame.draw.rect(DISPLAYSURF,RED,(i*((DISPLAY_SIZE[0]/8*7)/len(board)) +1,g*((DISPLAY_SIZE[1])/len(board[i])) + 1,(DISPLAY_SIZE[0] / 8 * 7) / len(board) - 1,(DISPLAY_SIZE[1] - 20)/len(board[i]) - 1), 0)
+            pygame.draw.circle(DISPLAYSURF,
+                                colours[board[i][g] if board[i][g] != 3 else board[i][g] - 1],
+                                ( i * ((DISPLAY_SIZE[0] / 8 * 7) / len(board)) + 1 + (((DISPLAY_SIZE[0] / 8 * 7) / len(board) - 1) / 2),
+                                 g * ((DISPLAY_SIZE[1]) / len(board[i])) +((DISPLAY_SIZE[1]) / len(board[i])- 1) / 2),
+                                ((DISPLAY_SIZE[0] / 8 * 7) / len(board)) / 2 - 2 if ((DISPLAY_SIZE[0] / 8 * 7) / len(board)) < ((DISPLAY_SIZE[1]) / len(board[i]) - 1) else ((DISPLAY_SIZE[1]) / len(board[i]) - 1) / 2 - 2, 0)
+            pygame.draw.rect(DISPLAYSURF,RED,(DISPLAY_SIZE[0]/8*7 + 1,1,DISPLAY_SIZE[0]/8-1,DISPLAY_SIZE[1]),0)
+    pygame.display.update()
+    for event in pygame.event.get():  
+        if event.type == pygame.QUIT:  running = False
+        if event.type == MOUSEBUTTONDOWN and((not pygame.mouse.get_pressed(num_buttons=5)[3]) or (not pygame.mouse.get_pressed(num_buttons=5)[4])):
+            if pygame.mouse.get_pressed(num_buttons=3)[0]: set = 1
+            elif pygame.mouse.get_pressed(num_buttons=3)[1]: set = 0
+            elif pygame.mouse.get_pressed(num_buttons=3)[2]: set = 3
+            mouseloc = pygame.mouse.get_pos()
+            for i in range(0,len(board)):
+                for g in range(0,len(board[i])):
+                    if mouseloc[0]>=i*((DISPLAY_SIZE[0]/8*7)/len(board)) +1 and mouseloc[0]<=i*((DISPLAY_SIZE[0]/8*7)/len(board)) +1 + (DISPLAY_SIZE[0] / 8 * 7) / len(board) - 1 and mouseloc[1] >= g*((DISPLAY_SIZE[1])/len(board[i])) + 1 and mouseloc[1] <= g*((DISPLAY_SIZE[1]-20)/len(board[i])) + 1 + (DISPLAY_SIZE[1] - 20)/len(board[i]) - 1: board[i][g]=set
+            if mouseloc[0]>=DISPLAY_SIZE[0]/8*7 + 1 and mouseloc[0] <= DISPLAY_SIZE[0]/8*7 + 1+ DISPLAY_SIZE[0]/8-1 and mouseloc[1]>=1 and mouseloc[1]<=1+DISPLAY_SIZE[1]: board = solve(board)
+
